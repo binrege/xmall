949c7bdf80891cd1bb73d67baafeb81eb7a7b4a0
binrege <752228158@qq.com>
2020-01-15 09:24:18 +0800

    上传测试

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home">
+    设置图标
+    <div class="iconfont icon-shezhi"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      siderList:[
+        {
+
+        }
+      ]
+    };
+  },
+  props: {},
+  components: {},
+  methods: {},
+  mounted() {},
+  watch: {},
+  computed: {}
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
